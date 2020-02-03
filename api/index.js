98744ff3c58eb07125d8fcb52e9d3810a83cd393
Aleksandr Gornostal <agornostal@star.global>
2020-02-03 18:52:31 +0200

    #5 debugging

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
